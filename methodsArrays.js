96cb7ed7f457a8b94d2cd70d6cccbea54ee0032b
Cleiton Mariano <cleitonomariano@gmail.com>
2023-09-07 15:13:30 -0300

    methods class1

---
+++ methodsArrays.js
@@ -0,0 +1,22 @@
+// Crinado Arrays 
+
+// const myArrayLiteral = [1, 2, 3] // Forma literal
+// const MyArrayConstructorInstance = new Array(1,2,3)
+// const myArrayConstuctorCall = Array(1,2,3)
+// const myArrayFrom = Array.from(myArrayLiteral)
+// const myArraySpread = [...myArrayFrom]
+
+// ""
+// console.log(myArraySpread)
+
+const arr = [1]
+
+arr.push(2, 10, 11)
+
+arr.unshift(10)
+
+arr.shift()
+
+arr.pop()
+
+console.log(arr)
