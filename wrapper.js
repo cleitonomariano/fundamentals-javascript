9174e9a41d5c9de7ff00e3b2323fd9bb9a3c5330
Cleiton Mariano <cleitonomariano@gmail.com>
2023-09-07 11:59:25 -0300

    wrapper functions

---
+++ wrapper.js
@@ -0,0 +1,29 @@
+// // Criar uma string
+// "brazilian Dev1 ".split("")
+
+// const str1 = String("Brazilian Dev1");
+// const str2 = new String("Brazilian Dev2");
+// console.log(`str1 Dividida ${str1.split("")} str2 Dividida ${str2.split("")}`)
+
+
+// const str2Convertido = str2.valueOf();
+// console.log(typeof str2Convertido)
+
+// Create new Number
+// const num1 = Number('1')
+// const num2 = new Number('2')
+
+// console.log(`num1 ${num1} num2 ${num2}`)
+
+// console.log(`num1 com duas casas decimais ${ num1.toFixed(2)}
+// num2 com três casas decimais ${num2.toFixed(3)}`)
+
+// Create a Boolean
+
+const bool1 = Boolean(true)
+const bool2 = Boolean('')
+
+console.log(`bool1 ${bool1} bool2 ${bool2}`)
+
+console.log(`bool1.toString() = ${bool1.toString()} 
+bool2.toString() ${bool2.toString()}`)
